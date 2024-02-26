d374803ba27d7613980d330ca47d115de6b6a1cf
Harsha <dharsha227@gmail.com>
2024-02-26 08:20:23 +0530

    driver login and dashboard

--- src/Screens/DriverDashboard.tsx
+++ src/Screens/DriverDashboard.tsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import ButtonComp from "../Components/ButtonComp";
+import MySupClient from "../SupabaseClient";
+import { useNavigate } from "react-router-dom";
+
 export default function DriverDashboard() {
+    const [supabase] = useState(() => MySupClient());
+    const navigate = useNavigate();
     return (
         <div>
             Driver Dashboard
+            <br />
+            you have uploaded information
+            <br />
+            please wait for approval
+            <br />
+            <ButtonComp text="Logout" onClick={async () => {
+                var session = await supabase.auth.signOut()
+                console.log(session)
+                if (session.error) {
+                    alert(session.error.message)
+                    return
+                }
+                else {
+                    navigate('/')
+                }
+            }} />
         </div>
     )
 }
