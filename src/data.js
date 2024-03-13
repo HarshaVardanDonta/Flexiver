010f28cc91584288e78b3ea9365756fc42fa0ce3
kattachaithanyakumar <k.chaithanya278@gmail.com>
2024-03-14 00:44:04 +0530

    polished UI

--- src/data.js
+++ src/data.js
@@ -3,21 +3,21 @@ import house from "./Assets/home.gif";
 import scooty from "./Assets/scooty.gif";
 
 const services = [
-    {
-        title: "Package Delivery",
-        desc: "Swift and secure delivery for any package size, with real-time tracking.",
-        image: box,
-    },
-    {
-        title: "Home Moving",
-        desc: "Hassle-free moving with experienced movers, ensuring a smooth transition.",
-        image: house,
-    },
-    {
-        title: "Custom Solutions",
-        desc: "Tailored transportation solutions for your unique needs and projects.",
-        image: scooty,
-    }
+  {
+    title: "Package Delivery",
+    desc: "Swift and secure delivery with real-time tracking for packages of any size.",
+    image: box,
+  },
+  {
+    title: "Home Moving",
+    desc: "Hassle-free moving with experienced movers ensuring a smooth transition to your new home.",
+    image: house,
+  },
+  {
+    title: "Custom Solutions",
+    desc: "Tailored transportation solutions for your unique needs and projects, provided with efficiency and reliability.",
+    image: scooty,
+  },
 ];
 
 export { services };
