3fc882896e6ed35f5cf4085115dbcbe3dfd479bf
kattachaithanyakumar <k.chaithanya278@gmail.com>
2024-04-13 11:27:45 +0530

    camera on status update

--- src/Screens/DriverPortal/ActiveDeliveryScreen/ActiveDeliveryScreen.tsx
+++ src/Screens/DriverPortal/ActiveDeliveryScreen/ActiveDeliveryScreen.tsx
@@ -7,6 +7,9 @@ import MapComp from "../../../Components/MapComp";
 import { Icon } from "leaflet";
 import mark from "../../../Assets/Location.png";
 import pin from "../../../Assets/MapPin.png";
+import { useState } from "react";
+import ImagePreview from "../../../Components/ImagePreview";
+import Camera from "react-html5-camera-photo";
 
 const LocationIcon = new Icon({
   iconUrl: mark,
@@ -19,6 +22,43 @@ const PinIcon = new Icon({
 });
 
 export default function ActiveDeliveryScreen() {
+  const [showCameraButton, setShowCameraButton] = useState(false);
+  const [openCamera, setOpenCamera] = useState(false);
+  const [dataUri, setDataUri] = useState("");
+
+  const handleDropDownChange = (option: string) => {
+    // Check if the selected option is 'Package Picked Up' or 'Package Delivered'
+    if (option === "Package Picked Up" || option === "Package Delivered") {
+      setShowCameraButton(true); // Show camera button
+    } else {
+      setShowCameraButton(false); // Hide camera button
+    }
+  };
+
+  const handleTakePhotoAnimationDone = (dataUri: any) => {
+    console.log("Take Photo");
+    console.log(dataUri);
+    setDataUri(dataUri);
+  };
+
+  if (openCamera) {
+    return (
+      <div>
+        {dataUri ? (
+          <>
+            <ImagePreview dataUri={dataUri} isFullscreen={true} />
+            <button onClick={() => setOpenCamera(false)}>back</button>
+          </>
+        ) : (
+          <Camera
+            onTakePhotoAnimationDone={handleTakePhotoAnimationDone}
+            isFullscreen={true}
+          />
+        )}
+      </div>
+    );
+  }
+
   return (
     <div>
       <CustomerPortalHeader driverSide={true} />
@@ -72,9 +112,18 @@ export default function ActiveDeliveryScreen() {
             selectedOption={"Delivery Partner Assigned"}
             buttonId={"statusDropId"}
             menuId={"StatusDropMenuId"}
-            onOptionChange={function (option: string): void {}}
+            onOptionChange={handleDropDownChange}
           />
         </div>
+
+        {showCameraButton && (
+          <div
+            className="buttonStyle"
+            onClick={() => setOpenCamera(!openCamera)}
+          >
+            Take a Picture
+          </div>
+        )}
       </div>
     </div>
   );
